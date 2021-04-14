b28247c9ba7f41560e6db216415632a61f7e2e54
unknown <gs.coelho@outlook.com>
2021-04-14 15:23:21 -0300

    Comandos Básicos no JavaScript

---
+++ js/main.js
@@ -0,0 +1,69 @@
+/*var nome = "Guilherme Coelho";
+var n1 = parseInt(prompt("Digite um número:"));
+var n2 = parseInt(prompt("Digite um número:"));
+var frase = "Brasil"
+console.log(n1+n2);
+console.log(frase.replace("Brasil",nome))
+
+
+var lista = ["maçã","pera","banana","laranja"];
+lista.pop();
+alert(lista[0]);
+alert(lista.toString()[0]);
+console.log(lista.join(" | "));
+
+
+var fruta = {nome:"maçã", cor:"vermelha"};
+console.log(fruta);                         // Mostra todos elementos do array
+
+console.log(fruta.nome);                    // Mostra o item dentro do array que está dentro de nome
+
+
+var frutas = [{nome:"maçã", cor:"vermelha"},{nome:"uva",cor:"roxa"}, {nome: "melancia", cor:"verde"}]
+console.log(frutas);                         
+
+console.log(frutas[2].nome);   // Mostrará melancia
+
+
+for(count=0;count<=5;count++){
+    alert(count);
+}
+
+var d = new Date()
+alert(d.getMinutes())
+
+var a = parseInt(prompt("Digite o valor de n1"));
+var b = parseInt(prompt("Digite o valor de n2"));
+var c = parseFloat(prompt("Qual é a multiplicação?"))
+function soma(n1,n2,n3) {
+    return (n1 + n2)*n3 
+}
+
+console.log(soma(a,b,c));
+
+var dados = [3,4];
+var cat1 = parseInt(dados[0]);
+var cat2 = parseInt(dados[1]);
+
+function Hipotenusa(b,c){
+'    return Math.sqrt(Math.pow(b,2)+Math.pow(c,2));
+}
+console.log(Hipotenusa(cat1,cat2));
+
+
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado</b>";
+}
+function newPage(){
+    window.open("https://www.google.com.br/")
+}
+function newText(elemento){
+    elemento.innerHTML = "<p>Ekko é um campeão jogado na rota do meio ou na jungle, possui um kit de hard engage, ótimo para lutar em equipes</p>";
+}
+function voltar(elemento){
+    elemento.innerHTML = "Ekko é um campeão";
+}
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}'
+*/
